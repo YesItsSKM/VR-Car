23c904dbf861a32376d45c8cc50b1205bfc2e959
Shubham Kumar Maurya <yesitsskm@gmail.com>
2022-01-12 20:24:51 +0000

    Basic car movement and hand animations added.

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class PlayerInput : MonoBehaviour
+{
+    public GameObject player;
+    public Animator leftHandAnimator, rightHandAnimator;
+
+    [SerializeField] float moveSpeed = 0.2f;
+    [SerializeField] float rotateSpeed = 0.25f;
+
+    InputDevice leftController, rightController;
+
+    //int i = 0;
+    void Start()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Controller;
+        InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, devices);
+
+        leftController = devices[0];
+        rightController = devices[1];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AnimateHands();
+
+        if (leftController.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 carMoveValue) && carMoveValue != Vector2.zero)
+        {
+            Debug.Log(carMoveValue);
+
+            //car.transform.position = new Vector3(moveSpeed * carMoveValue.y + car.transform.position.x, car.transform.position.y, car.transform.position.z);
+
+            //car.transform.position += car.transform.forward;
+
+            player.transform.Translate(0f, 0f, -(carMoveValue.x + carMoveValue.y) * moveSpeed);
+        }
+
+        if (rightController.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 carRotateValue) && carRotateValue != Vector2.zero)
+        {
+            //car.transform.localEulerAngles = new Vector3(car.transform.rotation.x, car.transform.rotation.y + rotateSpeed, car.transform.rotation.z);
+
+            player.transform.Rotate(0f, carRotateValue.x * rotateSpeed, 0f);
+
+            Debug.Log(carRotateValue);
+        }
+    }
+
+    private void AnimateHands()
+    {
+        if (leftController.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+        {
+            leftHandAnimator.SetFloat("Trigger", triggerValue);
+        }
+        else
+        {
+            leftHandAnimator.SetFloat("Trigger", 0f);
+        }
+
+        if (leftController.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+        {
+            leftHandAnimator.SetFloat("Grip", gripValue);
+        }
+        else
+        {
+            leftHandAnimator.SetFloat("Grip", 0f);
+        }
+
+
+
+        if (rightController.TryGetFeatureValue(CommonUsages.trigger, out float rTriggerValue))
+        {
+            rightHandAnimator.SetFloat("Trigger", rTriggerValue);
+        }
+        else
+        {
+            rightHandAnimator.SetFloat("Trigger", 0f);
+        }
+
+        if (rightController.TryGetFeatureValue(CommonUsages.grip, out float rGripValue))
+        {
+            rightHandAnimator.SetFloat("Grip", rGripValue);
+        }
+        else
+        {
+            rightHandAnimator.SetFloat("Grip", 0f);
+        }
+    }
+}
